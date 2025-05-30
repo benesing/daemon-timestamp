148a2c1bf5f7a0851455c18600ee853ef17a793d
Ben Singer <bensingerco@gmail.com>
2025-05-30 15:47:15 -0400

    Add built files

---
+++ dist-esm/index.d.ts
@@ -0,0 +1,2 @@
+export declare function timestamp(): string;
+export default timestamp;
---
+++ dist-esm/index.js
@@ -0,0 +1,20 @@
+// © 2025 Ben Singer
+export function timestamp() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const monval = now.getMonth() + 1;
+    const mons = monval.toString().padStart(2, "0");
+    const days = now.getDate().toString().padStart(2, "0");
+    const hour = now.getHours().toString().padStart(2, "0");
+    const mins = now.getMinutes().toString().padStart(2, "0");
+    const secs = now.getSeconds().toString().padStart(2, "0");
+    const offset_amount = now.getTimezoneOffset();
+    const offset_positive = Math.abs(offset_amount);
+    const offset_hours = Math.floor(offset_positive / 60);
+    const offset_mins = offset_positive % 60;
+    const offsign = (offset_amount > 0 ? "-" : "+");
+    const offhour = offset_hours.toString().padStart(2, "0");
+    const offmins = offset_mins.toString().padStart(2, "0");
+    return `${year}-${mons}-${days} ${hour}:${mins}:${secs} ${offsign}${offhour}${offmins}`;
+}
+export default timestamp;
